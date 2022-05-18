9367e1c44653674c61554516e9cd9e943586bb59
nitesh-30 <niteshkumarmishra77838@gmail.com>
2022-05-18 08:10:05 +0530

    first commit

--- studyportal/settings.py
+++ studyportal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-07%2=df!!_!fon4*fk5twzvhy4!n_5#q-$0*3evyd=y26$p)!b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["e-educationnitesh.herokuapp.com","localhost"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
